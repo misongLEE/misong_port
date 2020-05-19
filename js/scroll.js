4600104839762d9baa6386a5cccff1ee1a46f837
misongLEE <58838492+misongLEE@users.noreply.github.com>
2020-05-19 18:12:05 +0900

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,3 +1,5 @@
+
+
 $(function(){
 
     // 전체 원페이지 휠스크롤 swiper
@@ -7,10 +9,6 @@ $(function(){
         spaceBetween: 0,
         mousewheel: true,
         speed : 1000,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
     });
 
     // 하단 푸터 메뉴 클릭시 해당 영역으로 이동
@@ -27,20 +25,3 @@ $(function(){
     });
 });
 
-
-/* 스크롤시 스크롤 페이드인 */
-$(document).ready(function() {
-    /* 1 */
-    $(window).scroll( function(){
-        /* 2 */
-        console.log(idx);
-        $('.hideme').each( function(i){
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            /* 3 */
-            if( bottom_of_window > bottom_of_object/2 ){
-                $(this).animate({'opacity':'1'},500);
-            }
-        }); 
-    });
-});
